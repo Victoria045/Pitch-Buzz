9fea8826ec4d1f97a83a0d7d9f1d02b6d8e1ec20
Victoria045 <victoriaberyl45@gmail.com>
2021-11-03 11:04:59 +0300

    create a virtual column to connect with the foreign key in the Comment model

--- app/models.py
+++ app/models.py
@@ -18,6 +18,9 @@ class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(255),nullable = False)
     post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
 
 
 class Comment(db.Model):
