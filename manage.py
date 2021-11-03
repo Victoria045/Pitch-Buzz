0b16e9c8955853f15dfba436df96e8878b773d65
Victoria045 <victoriaberyl45@gmail.com>
2021-11-03 08:16:33 +0300

    use @manager.command decorator to run unit tests

--- manage.py
+++ manage.py
@@ -6,7 +6,15 @@ from app.models import User
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
-if __name__ == '__main__':
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == "__main__":
     manager.run()
-
-
