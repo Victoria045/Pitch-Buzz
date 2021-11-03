7752a435c6b057334df90e1f032c72391183bc7d
Victoria045 <victoriaberyl45@gmail.com>
2021-11-03 10:52:44 +0300

    import flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .main import main as main_blueprint
 from config import config_options
 
 
@@ -19,11 +19,12 @@ def create_app(config_name):
     # db = SQLAlchemy(app)  
 
     # Registering the blueprint
-    app.register_blueprint(main_blueprint)
+    # from .main import main as main_blueprint
+    # app.register_blueprint(main_blueprint)
 
     # setting config
-    from .request import configure_request
-    configure_request(app)
+    # from .request import configure_request
+    # configure_request(app)
     
     # Will add the views and forms
 
