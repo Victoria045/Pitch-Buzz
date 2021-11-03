88cf2b1dfb16400ffed2ae1dd3a704e4d9f6d2ef
Victoria045 <victoriaberyl45@gmail.com>
2021-11-03 11:03:05 +0300

    create virtual columns in the User model to connect with the foreign key

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
