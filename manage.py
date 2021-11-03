1f560d2c6456928cc257ae1c6b1cca4f1678e74b
Victoria045 <victoriaberyl45@gmail.com>
2021-11-03 10:52:32 +0300

    create the app instance

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@ from flask_script import Manager, Server
 from app import create_app, db
 from app.models import User
 
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+
+manager.add_command('server',Server)
+
 
 @manager.shell
 def make_shell_context():
