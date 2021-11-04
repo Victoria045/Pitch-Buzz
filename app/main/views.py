ea09f732ca0adfd7f688ec628538cc9499ca3d30
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 07:43:29 +0300

    create profile view function

---
+++ app/main/views.py
@@ -0,0 +1,61 @@
+from flask import render_template, redirect, url_for,abort,request
+from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,PitchForm,CommentForm
+from .. import db,images
+
+@main.route('/')
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+def index():
+    pitches = Pitch.query.all()
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_pitch.html', form = form)
+
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
+
