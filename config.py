03079d1bcbd65f3905ce924ab02ad1dd02df1689
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 12:08:17 +0300

    set up email configurations

--- config.py
+++ config.py
@@ -13,6 +13,12 @@ class Config:
 
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    #  email configurations/connecting to Gmail
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
