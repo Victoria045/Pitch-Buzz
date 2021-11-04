48ae1b343e3bd74c6d97c89e525774fcc3df14fd
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 11:25:42 +0300

    modify the login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,9 +5,28 @@ from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
 
-@auth.route('/login')
+
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "PitchBuzz login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+# @auth.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     flash('You have been successfully logged out')
+#     return redirect(url_for("main.index"))
+
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
