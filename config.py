b36ed64ed579ed8393dac66ca271b826529240df
Victoria045 <victoriaberyl45@gmail.com>
2021-11-11 07:14:24 +0300

    modify config.py file

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class Config:
     '''
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://victoria:pass@localhost/pitch'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -35,7 +34,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://victoria:pass@localhost/pitch'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
